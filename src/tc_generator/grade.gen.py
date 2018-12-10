6b108e49399a6210fd6a7ecb00e69de9d575f665
DaRekaze <darekaze@gmail.com>
2018-12-11 02:27:59 +0800

    add gpa

---
+++ src/tc_generator/grade.gen.py
@@ -0,0 +1,74 @@
+import os
+import argparse
+import random
+import json
+
+grade = ['A+', 'A', 'B+', 'B', 'C+', 'C', 'D', 'F']
+
+
+def gpa(grades):
+    ap = a = bp = b = cp = c = d = 0
+
+    for grade in grades:
+        if grade == 'A+':
+            ap += 1
+        elif grade == 'A':
+            a += 1
+        elif grade == 'B+':
+            bp += 1
+        elif grade == 'B':
+            b += 1
+        elif grade == 'C+':
+            cp += 1
+        elif grade == 'C':
+            c += 1
+        elif grade == 'D':
+            d += 1
+
+    sum = float(ap * 4.5 + a * 4.0 + bp * 3.5 + b * 3.0
+                + cp * 2.5 + c * 2.0 + d * 1.0)
+
+    return sum / len(grades)
+
+
+def generate(cases, r, fileName='sorting', dir='testCases'):
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+
+    with open('{}/{}.json'.format(dir, fileName), 'w') as f:
+        outputs = []
+        for _ in range(cases):
+            li = {'input': []}
+            for _ in range(r):
+                li['input'].append(random.choice(grade))
+            li['result'] = gpa(li['input'])
+            outputs.append(li)
+        json.dump(outputs, f, indent=2)
+    print('Done!\n')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Random test case generator for sorting algorithms',
+    )
+    parser.add_argument(
+        '-c', '--tc',
+        default=30,
+        type=int,
+        help='Number of testcases'
+    )
+    parser.add_argument(
+        '-n', '--el',
+        default=5,
+        type=int,
+        help='Number of element inside each list'
+    )
+    parser.add_argument(
+        '--name',
+        default='grade',
+        type=str,
+        help='The name of the output [].json file (default: grade)'
+    )
+
+    args = parser.parse_args()
+    generate(args.tc, args.el, args.name)
---
+++ src/tmods/gpa_b12.py
@@ -0,0 +1,23 @@
+def gpa(grades):
+    ap = a = bp = b = cp = c = d = 0
+
+    for grade in grades:
+        if grade == 'A+':
+            ap += 2     # bug
+        elif grade == 'A':
+            a += 2      # bug
+        elif grade == 'B+':
+            bp += 2     # bug
+        elif grade == 'B':
+            b += 2      # bug
+        elif grade == 'C+':
+            cp += 2     # bug
+        elif grade == 'C':
+            c += 1
+        elif grade == 'D':
+            d += 2      # bug
+
+    sum = float(ap * 4.5 + a * 4.0 + bp * 3.5 + b * 3.0
+                + cp * 2.5 + c * 2.0 + d * 1.0)
+
+    return sum / len(grades)    # gpa
